b1d591a746a5b613450bf5a8a709a4c4dd7f4138
oweaj <qwe1520156@naver.com>
2023-08-01 00:18:53 +0900

    [FIX] 정규식 변경 및 id에서 type으로 조건 변경

--- src/hooks/useInput.jsx
+++ src/hooks/useInput.jsx
@@ -3,20 +3,22 @@ import { useState } from "react";
 const useInput = () => {
   const [data, setData] = useState("");
   const [state, setState] = useState(false);
-  const emailCheck = /[\w]+@[\w]$/;
+  const emailCheck = /[^\s]+@[^\s]/;
 
   const onChange = (e) => {
-    const { id, value } = e.target;
+    const { type, value } = e.target;
     setData(value);
 
-    if (id === "email-input") {
+    if (type === "email") {
       setState(emailCheck.test(value) ? true : false);
-    } else {
+    } else if (type === "password") {
       setState(value.length >= 8 ? true : false);
+    } else {
+      setState(false);
     }
   };
 
-  return { onChange, data, state };
+  return { onChange, data, setData, state };
 };
 
 export default useInput;
