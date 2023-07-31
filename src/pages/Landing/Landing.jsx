dc81d45da407648c6bb540f42844f1262a7c4232
oweaj <qwe1520156@naver.com>
2023-08-01 00:17:43 +0900

    [FEAT] 랜딩페이지 추가 생성

--- src/pages/Landing/Landing.jsx
+++ src/pages/Landing/Landing.jsx
@@ -5,11 +5,11 @@ const Landing = () => {
   return (
     <div className="contentStyle gap-4">
       <Header title="TodoList" />
-      <Link to="/signin" className="buttonStyle flex items-center justify-center">
-        로그인
+      <Link to="/signin" className="linkStyle buttonStyle">
+        로그인 페이지로 이동
       </Link>
-      <Link to="/signup" className="buttonStyle flex items-center justify-center">
-        회원가입
+      <Link to="/signup" className="linkStyle buttonStyle ">
+        회원가입 페이지로 이동
       </Link>
     </div>
   );
