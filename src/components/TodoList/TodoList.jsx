c57ad6ad410ddf856dc6b4977356bfdf9385d063
oweaj <qwe1520156@naver.com>
2023-08-01 02:21:33 +0900

    [CHORE] edit input 누락 id 추가

--- src/components/TodoList/TodoList.jsx
+++ src/components/TodoList/TodoList.jsx
@@ -7,7 +7,7 @@ const TodoList = ({ item, handleEditMode, updateCheck, deleteTodo, updateTodo, c
     <li key={item.id} className="flex gap-1">
       <label className="flex items-center gap-1">
         <input type="checkbox" data-testid="modify-input" defaultChecked={item.isCompleted} className="w-5 h-5" onClick={() => updateCheck(item)} />
-        {item.id !== checkID ? <span>{item.todo}</span> : <input type="text" className="w-full h-full border border-gray-400 pl-2" onChange={newTodo.onChange} />}
+        {item.id !== checkID ? <span>{item.todo}</span> : <input type="text" data-testid="modify-input" className="w-full h-full border border-gray-400 pl-2" onChange={newTodo.onChange} />}
       </label>
       {item.id !== checkID ? (
         <div className="flex gap-2">
